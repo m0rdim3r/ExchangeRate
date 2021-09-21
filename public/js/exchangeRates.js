4fee22775e8f9cc9cdc3f8c581da2489e1090607
Robert Gralicki <robert.gralicki@gmail.com>
2021-09-21 11:17:10 +0200

    adding favicon.ico and auto close alert

--- public/js/exchangeRates.js
+++ public/js/exchangeRates.js
@@ -5,4 +5,10 @@ $(function () {
             url: 'https://cdn.datatables.net/plug-ins/1.11.2/i18n/pl.json'
         },
     });
+
+    window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+        });
+    }, 4000);
 });
